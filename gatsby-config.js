9b61b52df21d092ede05655b8db93e311a87017a
hector4213 <hectorpwns@gmail.com>
2021-01-25 21:54:43 -0500

    change browser color

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,8 +76,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#43464b`,
+        theme_color: `#43464b`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
